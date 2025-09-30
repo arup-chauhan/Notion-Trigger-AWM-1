2b359b8531096351d1570ca0f73b642cc594384a
Arup Chauhan <arupchauhan.connect@gmail.com>
2025-09-30 12:10:49 -0500

    Added yml IST
    Signed-off-by: Arup Chauhan <arupchauhan.connect@gmail.com>

--- main.py
+++ main.py
@@ -19,13 +19,28 @@ today = datetime.date.today().isoformat()
 data = {
     "parent": {"database_id": DATABASE_ID},
     "properties": {
+
+
+        # Date
         "Date": {"date": {"start": today}},
-        "In": {"number": 0},
-        "Out": {"number": 0},
+
+        # Number fields
+        "Cash In": {"number": 0},
+        "Cash Out": {"number": 0},
+
+        # Text fields
         "Purpose of Out": {"rich_text": [{"text": {"content": "N/A"}}]},
         "Purpose of In": {"rich_text": [{"text": {"content": "N/A"}}]},
+
+        # Account info
         "Account From": {"rich_text": [{"text": {"content": "N/A"}}]},
-        "Account To": {"rich_text": [{"text": {"content": "N/A"}}]}
+        "Account To": {"rich_text": [{"text": {"content": "N/A"}}]},
+
+        # Title field
+        "Transaction Type": {
+            "title": [{"text": {"content": f"Daily Cash Flow – {today}"}}]
+        },
+
     }
 }
 
